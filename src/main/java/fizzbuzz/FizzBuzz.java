bb11455294cf5d4a51a0411b330f66799ae7118b
Virjanand Panday <virjanand@virjanand.com>
2022-07-10 07:13:39 +0200

    Green Test: replace 3 with Fizz

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -6,6 +6,13 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public List<String> getNumbers(int from, int until) {
-        return IntStream.rangeClosed(from, until).mapToObj(Integer::toString).collect(Collectors.toList());
+        return IntStream.rangeClosed(from, until).mapToObj(i -> toFizzBuzz(i)).collect(Collectors.toList());
+    }
+
+    private String toFizzBuzz(int i) {
+        if (i == 3) {
+            return "Fizz";
+        }
+        return Integer.toString(i);
     }
 }
