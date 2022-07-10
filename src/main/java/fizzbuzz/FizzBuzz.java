0a84ea03e21ce8d761bb503412ca75c2dc26607a
Virjanand Panday <virjanand@virjanand.com>
2022-07-10 07:14:57 +0200

    Refactor: replace 3 with Fizz

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -6,13 +6,13 @@ import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public List<String> getNumbers(int from, int until) {
-        return IntStream.rangeClosed(from, until).mapToObj(i -> toFizzBuzz(i)).collect(Collectors.toList());
+        return IntStream.rangeClosed(from, until).mapToObj(this::toFizzBuzz).collect(Collectors.toList());
     }
 
-    private String toFizzBuzz(int i) {
-        if (i == 3) {
+    private String toFizzBuzz(int number) {
+        if (number == 3) {
             return "Fizz";
         }
-        return Integer.toString(i);
+        return Integer.toString(number);
     }
 }
--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -1,20 +1,26 @@
 package fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void createFizzBuzz() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
     void get100Numbers() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertThat(fizzBuzz.getNumbers(1, 100)).hasSize(100);
     }
 
     @Test
     void replace3withFizz() {
-        FizzBuzz fizzBuzz = new FizzBuzz();
         assertThat(fizzBuzz.getNumbers(1, 3).get(2)).isEqualTo("Fizz");
     }
 }
