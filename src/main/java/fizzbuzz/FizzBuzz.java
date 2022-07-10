cb9207db9322a31ac1b2586423525bf2491ae604
Virjanand Panday <virjanand@virjanand.com>
2022-07-10 07:09:21 +0200

    Green test: get 100 numbers

--- src/main/java/fizzbuzz/FizzBuzz.java
+++ src/main/java/fizzbuzz/FizzBuzz.java
@@ -1,9 +1,11 @@
 package fizzbuzz;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class FizzBuzz {
     public List<String> getNumbers(int i) {
-        return null;
+        return IntStream.rangeClosed(1, 100).mapToObj(Integer::toString).collect(Collectors.toList());
     }
 }
